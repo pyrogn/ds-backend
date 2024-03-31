7024aa4b4ca64915bd0ed2e9d8ed4636610a36ce
Your Name <you@example.com>
2024-03-31 17:41:39 +0000

    Add client.py, add demo script with demo commands

--- src/app.py
+++ src/app.py
@@ -1,56 +1,54 @@
 from flask import Flask, jsonify, request
-import requests
+from client import ImageProviderClient, PlateReaderClient
 import logging
-from models.plate_reader import PlateReader
-import io
 
-logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] [%(levelname)s] %(message)s')
 
 app = Flask(__name__)
-plate_reader = PlateReader.load_from_file("/app/model_weights/plate_reader_model.pth")
+image_provider_client = ImageProviderClient('http://178.154.220.122:7777/images')
+plate_reader_client = PlateReaderClient("/app/model_weights/plate_reader_model.pth")
 
-def download_image(img_id):
-    url = f"http://178.154.220.122:7777/images/{img_id}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return io.BytesIO(response.content)
-    else:
-        return None
+def handle_image_error(error_code: int) -> tuple[dict, int]:
+    error_messages = {
+        404: "Image not found",
+        408: "Timeout when downloading image",
+        500: "Failed to download image due to server error"
+    }
+    return {"error": error_messages.get(error_code, "Unknown error")}, error_code
 
 @app.route('/read_plate_single', methods=['GET'])
 def read_plate_single():
+    """Handler for single image id."""
     img_id = request.args.get('img_id')
-    if img_id is None:
-        logging.error('img_id parameter is required')
+    if not img_id:
         return jsonify({"error": "img_id parameter is required"}), 400
     
-    img_stream = download_image(img_id)
-    if img_stream is None:
-        logging.error('Failed to download image')
-        return jsonify({"error": "Failed to download image"}), 500
+    img_stream, error_code = image_provider_client.get_image(int(img_id))
+    if error_code:
+        return handle_image_error(error_code)
     
-    plate_number = plate_reader.read_text(img_stream)
-    return jsonify({"plate_number": plate_number})
+    plate_number = plate_reader_client.read_plate_number(img_stream)
+    return {"plate_number": plate_number}
 
 @app.route('/read_plate_multiple', methods=['POST'])
 def read_plate_multiple():
+    """Handler for multiple images id."""
     img_ids = request.json.get('img_ids')
     if not img_ids:
-        logging.error('img_ids parameter is required')
         return jsonify({"error": "img_ids parameter is required"}), 400
 
     results = []
     for img_id in img_ids:
-        img_stream = download_image(img_id)
-        if img_stream is None:
-            results.append({"img_id": img_id, "error": "Failed to download image"})
+        img_stream, error_code = image_provider_client.get_image(img_id)
+        if error_code: # if error, add error message
+            result = {"img_id": img_id}
+            result.update(handle_image_error(error_code)[0])
+            results.append(result)
             continue
-        plate_number = plate_reader.read_text(img_stream)
+        plate_number = plate_reader_client.read_plate_number(img_stream)
         results.append({"img_id": img_id, "plate_number": plate_number})
 
     return jsonify(results)
 
 if __name__ == '__main__':
-    # logging.basicConfig(level=logging.INFO)
-    logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s')
     app.run(host='0.0.0.0', port=8080, debug=True)
